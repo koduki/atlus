d6dd1477d628d053035848f443f027081cde60c6
koduki <pascalm3@gmail.com>
2012-06-02 16:48:58 +0900

    project init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atlus::Application.config.secret_token = '627256f7e05179c2e1fd181b5caa4c090991efdd69296cb5da47902ad3e414ed804fd35359c40de7ed9ff80b2a177aeb6db747374537abe3e2b7d75fc0a21a31'
