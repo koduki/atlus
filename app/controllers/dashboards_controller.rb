5b74d2ce4c1d6f55abd91a210d3fce8713fea54d
koduki <pascalm3@gmail.com>
2012-06-03 00:36:33 +0900

    fix ui.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -13,6 +13,7 @@ class DashboardsController < ApplicationController
   # GET /dashboards/1
   # GET /dashboards/1.json
   def show
+    @dashboards = Dashboard.all
     @dashboard = Dashboard.find(params[:id])
 
     respond_to do |format|
